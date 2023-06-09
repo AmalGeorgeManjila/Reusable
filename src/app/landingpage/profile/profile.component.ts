2b3e1e446e912bd04ef6c345ad7655f9aec0cbc7
Amal GEORGE <amal.george@tnpconsultants.com>
2023-06-09 09:56:41 +0530

    completed first design

--- src/app/landingpage/profile/profile.component.ts
+++ src/app/landingpage/profile/profile.component.ts
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class ProfileComponent {
 
 
-  
+  activeprofile:any=["../../../assets/Rectangle 43lady1.png","../../../assets/Rectangle 44lady2.png","../../../assets/Rectangle 45lady3.png","../../../assets/Rectangle 46lady5.png","../../../assets/Rectangle 47lady4.png","../../../assets/Rectangle 43lady1.png","../../../assets/Rectangle 44lady2.png","../../../assets/Rectangle 45lady3.png","../../../assets/Rectangle 46lady5.png","../../../assets/Rectangle 47lady4.png"];
+
+  activeones:number=3;
 
   
   
